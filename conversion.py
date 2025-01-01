66632a34b380f504ee935ad3b217794b285fc06e
Candestor <pravesh_1996@outlook.com>
2025-01-01 21:19:23 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,16 @@
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = 100
+#convert num1 into an integer
+int(num1)
+print(int(num1))
+#convert num2 into a float
+float(num2)
+print(float(num2))
+#convert num3 into a string
+str(num3)
+print(str(num3))
+#convert string1 into an integer
+int(string1)
+print(int(string1))
